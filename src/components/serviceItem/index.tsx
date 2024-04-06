9ccaecaf68f8f9fe2fa4afcb754dc4845bbc5e48
elie-imario <elieimario1@gmail.com>
2024-04-06 22:57:21 +0300

    ainmate service item on scroll

--- src/components/serviceItem/index.tsx
+++ src/components/serviceItem/index.tsx
@@ -1,14 +1,51 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
+import ScrollReveal from "scrollreveal";
+import {
+  fromLeftReveal,
+  fromRightReveal,
+} from "../../utils/scrollReveal.config";
 import "./serviceItem.scss";
 
 type Props = {
   serviceName: string;
   serviceImg: string;
+  serviceKey: number;
 };
 
-export const ServiceItem: FC<Props> = ({ serviceName, serviceImg }) => {
+export const ServiceItem: FC<Props> = ({
+  serviceName,
+  serviceImg,
+  serviceKey,
+}) => {
+  const serviceRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (
+      serviceRef.current?.className.indexOf("service-0") !== -1 ||
+      serviceRef.current?.className.indexOf("service-2") !== -1
+    ) {
+      ScrollReveal().reveal(
+        serviceRef.current?.querySelector(".serviceName") as HTMLElement,
+        fromLeftReveal
+      );
+
+      ScrollReveal().reveal(
+        serviceRef.current?.querySelector(".serviceImg") as HTMLElement,
+        fromRightReveal
+      );
+    } else {
+      ScrollReveal().reveal(
+        serviceRef.current?.querySelector(".serviceName") as HTMLElement,
+        fromRightReveal
+      );
+
+      ScrollReveal().reveal(
+        serviceRef.current?.querySelector(".serviceImg") as HTMLElement,
+        fromLeftReveal
+      );
+    }
+  }, []);
   return (
-    <div className="service-item">
+    <div className={`service-item service-${serviceKey}`} ref={serviceRef}>
       <span className="serviceName">{serviceName}</span>
       <div className="serviceImg">
         <img src={serviceImg} alt={serviceName} />
