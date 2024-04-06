719de0a1a6254446e553379118a4bbfed7ac504a
elie-imario <elieimario1@gmail.com>
2024-04-06 22:57:03 +0300

    animate project item on scroll

--- src/components/projectItem/index.tsx
+++ src/components/projectItem/index.tsx
@@ -1,7 +1,19 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import { Language } from "../../services/types/dataTypes";
 import { createMedia } from "@artsy/fresnel";
 import { Meteors } from "../ui/meteor";
+import ScrollReveal from "scrollreveal";
+import {
+  fromLeftReveal,
+  fromLeftReveal1,
+  fromLeftReveal2,
+  fromLeftReveal3,
+  fromRightReveal,
+  fromRightReveal1,
+  fromRightReveal2,
+  fromRightReveal3,
+  fromRightRevealImg,
+} from "../../utils/scrollReveal.config";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolder } from "@fortawesome/free-regular-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -13,6 +25,7 @@ type Props = {
   projectDescription: string;
   linktoGitHub: string;
   languages: Language[];
+  projectKey: number;
 };
 
 const { MediaContextProvider, Media } = createMedia({
@@ -29,12 +42,100 @@ export const ProjectItem: FC<Props> = ({
   projectDescription,
   linktoGitHub,
   languages,
+  projectKey,
 }) => {
+  const projectSmRef = useRef<HTMLDivElement>(null);
+  const projectXlRef = useRef<HTMLDivElement>(null);
+  const projectXlImgRef = useRef<HTMLDivElement>(null);
+
+  //Media at 768px and down
+  useEffect(() => {
+    if (
+      projectSmRef.current?.className.indexOf(" project-sm-0") !== -1 ||
+      projectSmRef.current?.className.indexOf(" project-sm-2") !== -1
+    ) {
+      ScrollReveal().reveal(
+        projectSmRef.current as HTMLElement,
+        fromLeftReveal
+      );
+    } else {
+      ScrollReveal().reveal(
+        projectSmRef.current as HTMLElement,
+        fromRightReveal
+      );
+    }
+  }, []);
+
+  //Media at 768px and up
+  useEffect(() => {
+    if (
+      projectXlRef.current?.className.indexOf("project-xl-0") !== -1 ||
+      projectXlRef.current?.className.indexOf("project-xl-2") !== -1
+    ) {
+      ScrollReveal().reveal(
+        projectXlRef.current?.querySelector(".project-name") as HTMLElement,
+        fromLeftReveal1
+      );
+      ScrollReveal().reveal(
+        projectXlRef.current?.querySelector(
+          ".project-description"
+        ) as HTMLElement,
+        fromLeftReveal2
+      );
+      ScrollReveal().reveal(
+        projectXlRef.current?.querySelector(".project-language") as HTMLElement,
+        fromLeftReveal3
+      );
+      ScrollReveal().reveal(
+        projectXlRef.current?.querySelector(".project-links") as HTMLElement,
+        fromLeftReveal3
+      );
+    } else {
+      ScrollReveal().reveal(
+        projectXlRef.current?.querySelector(".project-name") as HTMLElement,
+        fromRightReveal1
+      );
+      ScrollReveal().reveal(
+        projectXlRef.current?.querySelector(
+          ".project-description"
+        ) as HTMLElement,
+        fromRightReveal2
+      );
+      ScrollReveal().reveal(
+        projectXlRef.current?.querySelector(".project-language") as HTMLElement,
+        fromRightReveal3
+      );
+      ScrollReveal().reveal(
+        projectXlRef.current?.querySelector(".project-links") as HTMLElement,
+        fromRightReveal3
+      );
+    }
+  }, []);
+  //Media at 768px and up for Img
+  useEffect(() => {
+    if (
+      projectXlImgRef.current?.className.indexOf("project-xl-0") !== -1 ||
+      projectXlImgRef.current?.className.indexOf("project-xl-2") !== -1
+    ) {
+      ScrollReveal().reveal(
+        projectXlImgRef.current as HTMLElement,
+        fromRightRevealImg
+      );
+    } else {
+      ScrollReveal().reveal(
+        projectXlImgRef.current as HTMLElement,
+        fromLeftReveal2
+      );
+    }
+  }, []);
   return (
     <div className="project-item">
       <MediaContextProvider>
         <Media at="sm">
-          <div className="w-full relative">
+          <div
+            className={`w-full relative project-sm-${projectKey}`}
+            ref={projectSmRef}
+          >
             <div className="project-details relative overflow-hidden">
               <div className="project-links">
                 <div className="ico">
@@ -69,7 +170,10 @@ export const ProjectItem: FC<Props> = ({
           </div>
         </Media>
         <Media greaterThan="sm">
-          <div className="project-details">
+          <div
+            className={`project-details project-xl-${projectKey}`}
+            ref={projectXlRef}
+          >
             <div className="project-name">
               <span>{projectName}</span>
             </div>
@@ -99,7 +203,10 @@ export const ProjectItem: FC<Props> = ({
               </div>
             </div>
           </div>
-          <div className="project-img">
+          <div
+            className={`project-img project-xl-${projectKey}`}
+            ref={projectXlImgRef}
+          >
             <img src={projectScreen} alt={projectName} />
           </div>
         </Media>
