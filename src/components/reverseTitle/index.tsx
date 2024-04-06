f239e13f2ba19336ca1ec5435e1273c8371bb5e5
elie-imario <elieimario1@gmail.com>
2024-04-06 22:57:42 +0300

    animate reversetitle on scroll

--- src/components/reverseTitle/index.tsx
+++ src/components/reverseTitle/index.tsx
@@ -1,4 +1,6 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
+import ScrollReveal from "scrollreveal";
+import { ReverseTitleRevealConfig } from "../../utils/scrollReveal.config";
 import "./reverseTitle.scss";
 type Props = {
   ico: string;
@@ -6,8 +8,15 @@ type Props = {
 };
 
 export const ReverseTitle: FC<Props> = ({ ico, titleLead }) => {
+  const reverseTitleRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    ScrollReveal().reveal(
+      reverseTitleRef.current as HTMLElement,
+      ReverseTitleRevealConfig
+    );
+  }, []);
   return (
-    <div className="reverse-Title">
+    <div className="reverse-Title" ref={reverseTitleRef}>
       <div className="textline">
         <div className="content">
           <span className="ico">{ico}</span>
