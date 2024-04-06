7c915f70726a118e6a6987f3f295503e998c4e63
elie-imario <elieimario1@gmail.com>
2024-04-06 22:56:48 +0300

    animate items on scroll

--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -1,4 +1,6 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
+import ScrollReveal from "scrollreveal";
+import { ItemRevealConfig } from "../../utils/scrollReveal.config";
 import "./item.scss";
 
 type Props = {
@@ -6,8 +8,12 @@ type Props = {
   languageIcon: string;
 };
 export const Item: FC<Props> = ({ language, languageIcon }) => {
+  const itemRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    ScrollReveal().reveal(itemRef.current as HTMLElement, ItemRevealConfig);
+  }, []);
   return (
-    <div className="icon-item">
+    <div className="icon-item" ref={itemRef}>
       <div className="icon-img">
         <img src={languageIcon} alt={language} />
       </div>
