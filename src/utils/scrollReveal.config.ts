aa794ebc16d499fe00714775adcafaa9148a8fdf
elie-imario <elieimario1@gmail.com>
2024-04-06 22:56:08 +0300

    setting up scrollreveal config

---
+++ src/utils/scrollReveal.config.ts
@@ -0,0 +1,158 @@
+export const HeroRevealConfig = {
+  origin: "bottom",
+  duration: 1500,
+  delay: 250,
+  distance: "500px",
+  reset: true,
+  scale: 1,
+  easing: "ease",
+};
+
+export const TitleRevealConfig = {
+  origin: "left",
+  duration: 1500,
+  delay: 250,
+  distance: "500px",
+  scale: 1,
+  easing: "ease",
+};
+
+export const ReverseTitleRevealConfig = {
+  origin: "bottom",
+  duration: 1500,
+  delay: 300,
+  distance: "100px",
+  scale: 1,
+  easing: "ease",
+};
+
+export const contentsReveal = {
+  origin: "right",
+  duration: 1500,
+  delay: 350,
+  distance: "500px",
+  scale: 1,
+  easing: "ease",
+};
+
+export const ItemRevealConfig = {
+  origin: "bottom",
+  duration: 1600,
+  delay: 450,
+  interval: 200,
+  scale: 0,
+  easing: "ease",
+};
+
+export const fromLeftReveal = {
+  origin: "left",
+  duration: 1500,
+  delay: 350,
+  distance: "500px",
+  scale: 1,
+  easing: "ease",
+};
+export const fromRightReveal = {
+  origin: "right",
+  duration: 1500,
+  delay: 450,
+  distance: "500px",
+  scale: 1,
+  easing: "ease",
+};
+
+export const fromLeftReveal1 = {
+  origin: "left",
+  duration: 1500,
+  delay: 400,
+  distance: "500px",
+  scale: 1,
+  easing: "ease",
+};
+
+export const fromLeftReveal2 = {
+  origin: "left",
+  duration: 1500,
+  delay: 400,
+  distance: "1000px",
+  scale: 1,
+  easing: "ease",
+};
+
+export const fromLeftReveal3 = {
+  origin: "left",
+  duration: 1900,
+  delay: 450,
+  distance: "100%",
+  scale: 1,
+  easing: "ease",
+};
+export const fromLeftReveal4 = {
+  origin: "left",
+  duration: 2000,
+  delay: 500,
+  distance: "100%",
+  scale: 1,
+  easing: "ease",
+};
+
+export const fromRightReveal1 = {
+  origin: "right",
+  duration: 1500,
+  delay: 400,
+  distance: "500px",
+  scale: 1,
+  easing: "ease",
+};
+
+export const fromRightReveal2 = {
+  origin: "right",
+  duration: 1500,
+  delay: 400,
+  distance: "1000px",
+  scale: 1,
+  easing: "ease",
+};
+
+export const fromRightReveal3 = {
+  origin: "right",
+  duration: 1900,
+  delay: 450,
+  distance: "100%",
+  scale: 1,
+  easing: "ease",
+};
+export const fromRightReveal4 = {
+  origin: "right",
+  duration: 2000,
+  delay: 500,
+  distance: "100%",
+  scale: 1,
+  easing: "ease",
+};
+
+export const fromRightRevealImg = {
+  origin: "right",
+  duration: 1500,
+  delay: 400,
+  distance: "1000px",
+  scale: 1,
+  easing: "ease",
+};
+
+export const RevealLineConfig = {
+  origin: "bottom",
+  duration: 1500,
+  delay: 400,
+  interval: 200,
+  scale: 0,
+  easing: "ease",
+};
+export const RevealImgConfig = {
+  origin: "bottom",
+  duration: 1500,
+  delay: 400,
+  interval: 200,
+  scale: 0,
+  easing: "ease",
+};
